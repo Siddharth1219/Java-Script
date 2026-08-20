b4d0276b8e48e6d8fc7b338cbbad7a8e24c506a7
Siddharth Yadav <siddharth.obr@gmail.com>
2026-08-20 11:25:29 +0530

    Create variables.js

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 145338
+let accountEmail = "siddharth.obr@gmail.com"
+var accountPassword = "siddharth123"
+let accountCity = "Bangalore"
+let accountState;
+
+//accountId = 14 not allowed
+
+accountEmail = "sidd@gmail.com"
+accountPassword = "siddharth1234678"
+accountCity = "Bangalore, India"
+
+console.log(accountId);
+
+/* var is not used frequently in modern JavaScript, 
+but it is still valid. 
+It has function scope and can be redeclared and updated. */
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
