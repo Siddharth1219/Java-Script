9569fc7313d7b07c5fb4cbfa5490545fbb75c39b
Siddharth Yadav <siddharth.obr@gmail.com>
2026-08-23 08:51:57 +0530

    Create 9string.js

---
+++ .devcontainer/basics/9string.js
@@ -0,0 +1,14 @@
+const name = "siddharth"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");   ***normaly we can use this but it is not a good practice to use this because it is not readable and also it is not easy to maintain. so we can use template literals to make it more readable and maintainable.
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Call of Duty')
+console.log(gameName);
+console.log(gameName[0]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
