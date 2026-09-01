8df2c60687f115bc3a69d78e0d86d1a5350d3f0a
Siddharth Yadav <siddharth.obr@gmail.com>
2026-09-01 22:59:20 +0530

    Create 4_scopes_part2.js

---
+++ .devcontainer/basics3/4_scopes_part2.js
@@ -0,0 +1,45 @@
+function one() {
+    const username = "Siddharth"
+
+    function two() {
+        const website = "Siddharth.com"
+        console.log(username);
+
+    }
+    //console.log(website); // ReferenceError: website is not defined
+    two()
+}
+//one()
+
+
+
+if (true) {
+    const username = "Siddharth"
+    if(username === "Siddharth") {
+        const website = "Siddharth.com"
+        //console.log(username + website);
+    }
+    //console.log(username);
+}
+
+
+
+
+
+
+
+
+
+// **************************Interesting Example**************************
+function addone(num) {
+    return num + 1
+}
+addone(5) // 6
+
+
+
+const addtwo = function(num) {
+    return num + 2
+}
+
+addtwo(5) // 7
